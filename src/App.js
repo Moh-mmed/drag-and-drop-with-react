7fd7b21a8acac0142afde7bb855d4d177c733073
mohammedBnomr <moohaboy@gmail.com>
2022-04-11 12:56:46 +0300

    add drag and drop functions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+import DragNDrop from "./components/DragNDrop";
+
+const defaultData = [
+  { title: "group 1", items: ["1", "2", "3"] },
+  { title: "group 2", items: ["4", "5"] },
+];
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    if (localStorage.getItem("List")) {
+      setData(JSON.parse(localStorage.getItem("List")));
+    } else {
+      setData(defaultData);
+    }
+  }, [setData]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DragNDrop data={data} />
       </header>
     </div>
   );
---
+++ src/components/DragNDrop.js
@@ -0,0 +1,98 @@
+import React, { useState, useRef, useEffect } from "react";
+
+function DragNDrop({ data }) {
+  const [list, setList] = useState(data);
+  const [dragging, setDragging] = useState(false);
+
+  useEffect(() => {
+    setList(data);
+  }, [setList, data]);
+
+  const dragItem = useRef();
+  const dragItemNode = useRef();
+
+    // 1. Handle onDragStart
+  const handletDragStart = (e, item) => {
+    dragItemNode.current = e.target;
+    // the reason we add a dragend event listener here is that this event works only with the node we are dragging and not all nodes
+    dragItemNode.current.addEventListener("dragend", handleDragEnd);
+    dragItem.current = item;
+
+    setTimeout(() => {
+      setDragging(true);
+    }, 0);
+  };
+  const handleDragEnter = (e, targetItem) => {
+    if (dragItemNode.current !== e.target) {
+      setList((oldList) => {
+        let newList = JSON.parse(JSON.stringify(oldList));
+        newList[targetItem.grpI].items.splice(
+          targetItem.itemI,
+          0,
+          newList[dragItem.current.grpI].items.splice(
+            dragItem.current.itemI,
+            1
+          )[0]
+        );
+        dragItem.current = targetItem;
+        localStorage.setItem("List", JSON.stringify(newList));
+        return newList;
+      });
+    }
+  };
+  const handleDragEnd = (e) => {
+    setDragging(false);
+    dragItem.current = null;
+    dragItemNode.current.removeEventListener("dragend", handleDragEnd);
+    dragItemNode.current = null;
+  };
+  const getStyles = (item) => {
+    if (
+      dragItem.current.grpI === item.grpI &&
+      dragItem.current.itemI === item.itemI
+    ) {
+      return "dnd-item current";
+    }
+    return "dnd-item";
+  };
+
+  if (list) {
+    return (
+      <div className="drag-n-drop">
+        {list.map((grp, grpI) => (
+          <div
+            key={grp.title}
+            onDragEnter={
+              dragging && !grp.items.length
+                ? (e) => handleDragEnter(e, { grpI, itemI: 0 })
+                : null
+            }
+            className="dnd-group"
+          >
+            {grp.items.map((item, itemI) => (
+              <div
+                draggable
+                key={item}
+                onDragStart={(e) => handletDragStart(e, { grpI, itemI })}
+                onDragEnter={
+                  dragging
+                    ? (e) => {
+                        handleDragEnter(e, { grpI, itemI });
+                      }
+                    : null
+                }
+                className={dragging ? getStyles({ grpI, itemI }) : "dnd-item"}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
+
+export default DragNDrop;
